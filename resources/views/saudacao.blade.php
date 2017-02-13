fc27e658e68bed1f4aa9ee1481e794d25743cf87
Fabio Frascino <fabio@schenberg.com.br>
2017-02-13 21:17:04 -0200

    incluindo arquivo saudacao

---
+++ resources/views/saudacao.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Página de saudação</title>
+    <link rel="stylesheet" type="text/css" href="/css/app.css">
+  </head>
+  <body>
+    <div class="container">
+      <h1>Olá Mundo</h1>
+    </div>
+    <script type="text/javascript" src="/js/app.js"></script>
+  </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/saudacao', function () {
+    return view('saudacao');
+});
